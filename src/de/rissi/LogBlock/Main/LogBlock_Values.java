21f43ec9c4f49829196b7920e601fa1f908e079a
xBarkeeper <sandrokobe@web.de>
2017-12-04 13:44:58 +0100

    Test

---
+++ src/de/rissi/LogBlock/Main/LogBlock_Values.java
@@ -0,0 +1,13 @@
+package de.rissi.LogBlock.Main;
+
+import java.sql.Connection;
+
+import de.rissi.LogBlock.Database.MySQL;
+
+public class LogBlock_Values
+{
+	
+	public static final MySQL	MYSQL		= new MySQL("host.name", "port", "database", "user", "passwort");
+
+
+}
