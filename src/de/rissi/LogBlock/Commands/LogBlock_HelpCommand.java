f2cacb8a73f215974a355b52af057ecf8b032ff2
xBarkeeper <sandrokobe@web.de>
2018-02-14 13:13:52 +0100

    .

--- src/de/rissi/LogBlock/Commands/LogBlock_HelpCommand.java
+++ src/de/rissi/LogBlock/Commands/LogBlock_HelpCommand.java
@@ -10,7 +10,10 @@ public class LogBlock_HelpCommand
 	public static boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		Player p = (Player) sender;
 
-		p.sendMessage("pong");
+		p.sendMessage("LogBlock Hilfe:");
+		p.sendMessage("/lb gui ::: Enter/Leave Editor Mode");
+		p.sendMessage("/lb history ::: Get the History of a Coord");
+		p.sendMessage("/lb reset [] [] ::: Reset Area:");
 		
 
 		return true;
